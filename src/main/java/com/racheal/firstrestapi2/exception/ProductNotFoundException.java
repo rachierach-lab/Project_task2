5db3887c33431c2d05ae35be5ca4236533553076
Racheal Kanyinji <rachkan97@gmail.com>
2026-04-24 10:23:28 +0200

    Fix project structure and add images

---
+++ src/main/java/com/racheal/firstrestapi2/exception/ProductNotFoundException.java
@@ -0,0 +1,8 @@
+package com.racheal.firstrestapi2.exception;
+
+public class ProductNotFoundException extends RuntimeException {
+
+    public ProductNotFoundException(Long id) {
+        super("Product not found with id: " + id);
+    }
+}
